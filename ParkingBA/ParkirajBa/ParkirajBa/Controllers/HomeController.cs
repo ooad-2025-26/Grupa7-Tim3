f66be24c21d46b5bbd62832113be5f8207245022
tbeganovic2 <tbeganovic2@etf.unsa.ba>
2026-05-29 16:31:40 +0200

    Added Parking Repository

--- ParkingBA/ParkirajBa/ParkirajBa/Controllers/HomeController.cs
+++ ParkingBA/ParkirajBa/ParkirajBa/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParkirajBa.Data;
 using ParkirajBa.Models;
+using ParkirajBa.Repositories;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
@@ -17,11 +18,14 @@ namespace ParkirajBa.Controllers
 
         private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext database, IConfiguration configuration)
+        private readonly IParkingRepository _ParkingRepository;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext database, IConfiguration configuration, IParkingRepository parkingRepository)
         {
             _logger = logger;
             _database = database;
             _configuration = configuration;
+            _ParkingRepository = parkingRepository;
         }
 
         //db testing
@@ -211,53 +215,9 @@ namespace ParkirajBa.Controllers
         [HttpGet]
         public async Task<IActionResult> Search(string searchText, bool hasGarage, bool hasEVCharger, bool hasCameras,bool isDisabledAccessible,string regime, int maxPrice)
         {
-            // Start with all database records
-            var query = _database.ParkingObject.AsQueryable();
-
-            // Apply filters sequentially
-            if (!string.IsNullOrEmpty(searchText))
-            {
-                query = query.Where(p => p.name.Contains(searchText));
-            }
-
-            if (hasGarage)
-            {
-                query = query.Where(p => p.isUnderground??false);
-            }
-
-            if (hasEVCharger)
-            {
-                query = query.Where(p => p.hasEVCharger??false);
-            }
-
-            if (hasCameras)
-            {
-                query = query.Where(p => p.hasCameras??false);
-            }
-
-            if (isDisabledAccessible)
-            {
-                query = query.Where(p => p.isDisabledAccessible ?? false);
-            }
-
-            PricingType typeByRegime=PricingType.Hourly;
-            if (regime.Equals("Hour"))
-                typeByRegime = PricingType.Hourly;
-            else if (regime.Equals("Day"))
-                typeByRegime = PricingType.Daily;
-            else if (regime.Equals("Week"))
-                typeByRegime = PricingType.Weekly;
-            else if (regime.Equals("Month"))
-                typeByRegime = PricingType.Monthly;
-            else if(regime.Equals("Year"))
-                typeByRegime= PricingType.Yearly;
-
-
-            // Filter by maximum price
-            query = query.Where(p => _database.Pricing.Any(pricing => pricing.ParkingObjectID == p.ID && pricing.pricingType == typeByRegime && pricing.price < maxPrice));
-
             // Execute query and fetch data
-            var results = await query.ToListAsync();
+            var results = 
+                await _ParkingRepository.FilterParkings(searchText,hasGarage,hasEVCharger,hasCameras, isDisabledAccessible, regime, maxPrice);
 
             // Return filtered results as JSON to the frontend
             return Json(results);
---
+++ ParkingBA/ParkirajBa/ParkirajBa/Repositories/ParkingRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkirajBa.Data;
+using ParkirajBa.Models;
+
+namespace ParkirajBa.Repositories
+{
+    public class ParkingRepository:IParkingRepository
+    {
+        ApplicationDbContext _Database;
+
+        public ParkingRepository(ApplicationDbContext database)
+        {
+            this._Database = database;
+        }
+
+        public async Task<List<ParkingObject>>
+            FilterParkings(string searchText, bool hasGarage, bool hasEVCharger, bool hasCameras, bool isDisabledAccessible, string regime, int maxPrice)
+        {
+            var query = _Database.ParkingObject.AsQueryable();
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                query = query.Where(p => p.name.Contains(searchText));
+            }
+
+            if (hasGarage)
+            {
+                query = query.Where(p => p.isUnderground ?? false);
+            }
+
+            if (hasEVCharger)
+            {
+                query = query.Where(p => p.hasEVCharger ?? false);
+            }
+
+            if (hasCameras)
+            {
+                query = query.Where(p => p.hasCameras ?? false);
+            }
+
+            if (isDisabledAccessible)
+            {
+                query = query.Where(p => p.isDisabledAccessible ?? false);
+            }
+
+            PricingType typeByRegime = PricingType.Hourly;
+            if (regime.Equals("Hour"))
+                typeByRegime = PricingType.Hourly;
+            else if (regime.Equals("Day"))
+                typeByRegime = PricingType.Daily;
+            else if (regime.Equals("Week"))
+                typeByRegime = PricingType.Weekly;
+            else if (regime.Equals("Month"))
+                typeByRegime = PricingType.Monthly;
+            else if (regime.Equals("Year"))
+                typeByRegime = PricingType.Yearly;
+
+
+            // Filter by maximum price
+            query = query.Where(p => _Database.Pricing.Any(pricing => pricing.ParkingObjectID == p.ID && pricing.pricingType == typeByRegime && pricing.price < maxPrice));
+
+            // Execute query and fetch data
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<ParkingObject>
+            AddParking(ParkingObject NewParking)
+        {
+            _Database.ParkingObject.Add(NewParking);
+            
+            await _Database.SaveChangesAsync();
+
+            return NewParking;
+        }
+    }
+}
